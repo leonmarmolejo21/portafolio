2fb283075ed2705e7856131a5b0973c9b47c432b
leonmarmolejo21 <leon.e.marmolejo@gmail.com>
2025-04-28 12:50:53 -0600

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,37 @@
+function sumar(){
+	var n1=document.getElementById("num1").value;
+	var n2=document.getElementById("num2").value;
+	var res=parseFloat(n1)+parseFloat(n2);
+
+	document.getElementById('resultado').innerHTML=res;
+
+}
+
+
+function restar(){
+	var n1=document.getElementById("num1").value;
+	var n2=document.getElementById("num2").value;
+	var res=parseFloat(n1)-parseFloat(n2);
+
+	document.getElementById('resultado').innerHTML=res;
+
+}
+
+
+function multiplicar(){
+	var n1=document.getElementById("num1").value;
+	var n2=document.getElementById("num2").value;
+	var res=parseFloat(n1)*parseFloat(n2);
+
+	document.getElementById('resultado').innerHTML=res;
+
+}
+
+function dividir(){
+	var n1=document.getElementById("num1").value;
+	var n2=document.getElementById("num2").value;
+	var res=parseFloat(n1)/parseFloat(n2);
+
+	document.getElementById('resultado').innerHTML=res;
+
+}
